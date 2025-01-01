671fc28544692ae5075b5968b867889673571d36
Mahmoud Ibrahim <m.ibrahim@ensanydev.com>
2025-01-01 20:44:37 +0200

    add echo

---
+++ ecosystem.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    apps: [
+      {
+        name: "nuxt-app",
+        script: ".output/server/index.mjs",
+        instances: "max",
+        exec_mode: "cluster",
+        env: {
+          NODE_ENV: "production",
+          PORT: 3000,
+        },
+      },
+    ],
+  };
+  
