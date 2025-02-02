92de7b93eb9076889cbc3b1cc29843ebf45e24a7
omr <omradelbakry375@gmail.com>
2025-02-02 08:23:20 +0200

    designe dashboard charts

--- src/modules/charity/pages/Dashboard.vue
+++ src/modules/charity/pages/Dashboard.vue
@@ -9,12 +9,43 @@
     <div class="content mt-sm">
       <CharityStatistics />
 
-      <CharityCurrentCampaign />
+      <ClientOnly>
+        <!-- pie chart -->
+        <v-card elevation="0" class="p-4 mt-sm w-full">
+          <v-card-title>
+            <div class="flex gap-x-2 items-center mb-4">
+              <img src="../../../assets/images/line-chart.svg" alt="...." />
+              <h1 class="font-bold">
+                {{ $t("dashboard.donation_transuctions") }}
+              </h1>
+            </div>
+          </v-card-title>
+
+          <div class="flex justify-center">
+            <CharityPieChart />
+          </div>
+        </v-card>
+
+        <!-- line chart -->
+        <v-card elevation="0" class="p-4 mt-sm">
+          <v-card-title>
+            <div class="flex gap-x-2 items-center mb-4">
+              <img src="../../../assets/images/line-chart.svg" alt="...." />
+              <h1 class="font-bold">
+                {{ $t("dashboard.donation_growth") }}
+              </h1>
+            </div>
+          </v-card-title>
+
+          <CharityLineChart />
+        </v-card>
+      </ClientOnly>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { CharityLineChart } from "#components";
 import dashbordBreadcrumb from "~/global/dashbord-breadcrumb.vue";
 
 definePageMeta({
