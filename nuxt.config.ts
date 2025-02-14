cafbed69523d62bce249c200e22f5cb378a5bf0a
Mahmoud <m.ibrahim@ensanydev.com>
2025-02-14 18:17:44 +0200

    add google tag manager

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -20,6 +20,20 @@ export default defineNuxtConfig({
     head: {
       charset: "utf-8",
       viewport: "width=device-width, initial-scale=1",
+
+      script: [
+        {
+          type: 'text/javascript',
+          innerHTML: `
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-PWKGS9VZ');
+          `
+        },
+      ],
+
       link: [
         { rel: "icon", type: "image/ico", href: "/favicon.ico" },
         { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -38,6 +52,7 @@ export default defineNuxtConfig({
         },
       ],
     },
+
   },
 
   // custom components prefixes for auto importing
@@ -98,6 +113,7 @@ export default defineNuxtConfig({
   },
 
   modules: [
+
     "nuxt-lazytube",
     (_options, nuxt) => {
       nuxt.hooks.hook("vite:extendConfig", (config) => {
